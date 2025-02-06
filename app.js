661410757c96f7dd4bc7aa308e14403eeb58d620
Karina Rojas <karinacodecompetent@gmail.com>
2025-02-06 17:45:42 +0100

    Primer commit: Agregar proyecto de amigo secreto

---
+++ app.js
@@ -0,0 +1,15 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nameFriends = [];
+
+function agregarAmigo(){
+    let titulo = Document.querySelector('h1');
+    titulo.innerHTML = 'Amigo Secreto';
+
+    let seccion = Document.queryDelector('h2');
+    seccion = 'Digite el nombre de sus amigos';
+
+    let anadirNombre = document.getElementById('amigo');
+    anadirNombre.push();
+
+
+}
