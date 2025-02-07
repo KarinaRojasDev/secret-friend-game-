f16e4518b42badb6b7f81d27b00228a262ccbe17
Karina Rojas <karinacodecompetent@gmail.com>
2025-02-07 01:37:56 +0100

    Actualizo funcion agregar amigo y creo actualizar lista

--- app.js
+++ app.js
@@ -2,14 +2,36 @@
 let nameFriends = [];
 
 function agregarAmigo(){
-    let titulo = Document.querySelector('h1');
-    titulo.innerHTML = 'Amigo Secreto';
+    let titulo = document.querySelector('h1');
+    titulo.textContent = 'Amigo Secreto';
 
-    let seccion = Document.queryDelector('h2');
-    seccion = 'Digite el nombre de sus amigos';
+
+    let seccion = document.querySelector('h2');
+    if(seccion){
+        seccion.textContent = 'Digite el nombre de sus amigos';
+    }
+    
 
     let anadirNombre = document.getElementById('amigo');
-    anadirNombre.push();
+    let nombre = anadirNombre.value.trim();
+    if(nombre === ""){
+        alert("Por favor inserte un nombre");
+        return;
+    }else{
+        nameFriends.push(nombre);
+        actualizarLista();
+        anadirNombre.value = "";
+    }
+}
+function actualizarLista(){
 
+    let lista = document.querySelector('#listaAmigos');
+    lista.innerHTML = "listaAmigos";
 
+    nameFriends.forEach((nombre) => {
+        let item = document.createElement("li");
+        item.textContent = nombre;
+        lista.appendChild(item);
+
+    });
 }
