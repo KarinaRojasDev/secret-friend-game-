6ae7d468cad8165f22591b3893cc08966c73301d
Karina Rojas <karinacodecompetent@gmail.com>
2025-02-11 21:53:48 +0100

    Modificacion de js

--- app.js
+++ app.js
@@ -1,37 +1,47 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let nameFriends = [];
 
-function agregarAmigo(){
+function agregarAmigo() {
     let titulo = document.querySelector('h1');
     titulo.textContent = 'Amigo Secreto';
 
-
     let seccion = document.querySelector('h2');
-    if(seccion){
+    if (seccion) {
         seccion.textContent = 'Digite el nombre de sus amigos';
     }
-    
 
     let anadirNombre = document.getElementById('amigo');
     let nombre = anadirNombre.value.trim();
-    if(nombre === ""){
+
+    if (nombre === "") {
         alert("Por favor inserte un nombre");
         return;
-    }else{
-        nameFriends.push(nombre);
-        actualizarLista();
-        anadirNombre.value = "";
     }
-}
-function actualizarLista(){
 
+    nameFriends.push(nombre);
+    actualizarLista();
+    anadirNombre.value = "";
+}
+
+function actualizarLista() {
     let lista = document.querySelector('#listaAmigos');
-    lista.innerHTML = "listaAmigos";
+    lista.innerHTML = ""; 
 
     nameFriends.forEach((nombre) => {
         let item = document.createElement("li");
         item.textContent = nombre;
         lista.appendChild(item);
-
     });
+}
+
+function sortearAmigo() {
+    if (nameFriends.length === 0) {
+        alert("La lista está vacía");
+        return;
+    }
+
+    let aleatorio = Math.floor(Math.random() * nameFriends.length);
+    let amigoSorteado = nameFriends[aleatorio];
+
+    let elementoSorteo = document.getElementById('resultado');
+    elementoSorteo.innerHTML = `<li>${amigoSorteado}</li>`;
 }
